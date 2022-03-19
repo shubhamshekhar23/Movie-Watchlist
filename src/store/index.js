3936a77c9c58af44379fd1117ccdf3ab1bbd4f57
Shubham Shekhar <shubham.ae.iitkgp@gmail.com>
2022-03-19 20:32:35 +0530

    filter search and global store modules added

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,15 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import global from "./global";
+import filterSearch from "./filter-search";
+import Vuex from "vuex";
+import Vue from "vue";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+export default () => {
+  return new Vuex.Store({
+    modules: {
+      global,
+      filterSearch,
+    },
+  });
+};
