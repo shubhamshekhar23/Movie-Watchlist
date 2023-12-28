7a4db7aa5410170037eefa3782c4fa77eac25a14
Shubham Shekhar <shubham.ae.iitkgp@gmail.com>
2023-12-28 15:51:45 +0530

    config changes for deploy

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: [
-    'vuetify'
-  ]
-})
+  publicPath: "/Movie-Watchlist-Vue/",
+  transpileDependencies: ["vuetify"],
+});
