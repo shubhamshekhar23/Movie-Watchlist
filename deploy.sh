3ff8fb0fffede2b7dd6ef9538021b0c35aebb5da
Shubham Shekhar <shubham.ae.iitkgp@gmail.com>
2023-12-28 16:38:32 +0530

    changes

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Run build
+npm run build
+
+# Stage the dist directory
+git add dist -f
+
+# Commit changes
+git commit -am "deploying changes"
+
+# Push to gh-pages branch using git subtree
+git subtree push --prefix dist origin gh-pages
